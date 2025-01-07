f267d26d781a99a5c8a06f0d4d8e95aa58954ff5
Zual Rana <freelancerzual@gmail.com>
2025-01-08 00:19:13 +0600

    showing the modal

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-const fetchAllData = async (search) => {
+const fetchAllData = async (search, isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${search}`
   );
   const data = await res.json();
-  displayPhones(data.data);
+  displayPhones(data.data, isShowAll);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, isShowAll) => {
   const phoneContainer = document.getElementById("phone-container");
   phoneContainer.textContent = "";
   // show all btn
   const showAllBtn = document.getElementById("show-all");
-  if (phones.length > 12) {
+  if (phones.length > 12 && !isShowAll) {
     showAllBtn.classList.remove("hidden");
   } else {
     showAllBtn.classList.add("hidden");
   }
 
-  // if more 12 data show only 12 data
-  phones = phones.slice(0, 12);
+  // if more 12 data show only 12 data if not show all
+  if (!isShowAll) {
+    phones = phones.slice(0, 12);
+  }
 
   phones.forEach((phone) => {
     const div = document.createElement("div");
@@ -32,7 +34,7 @@ const displayPhones = (phones) => {
     <h2 class="card-title">${phone.phone_name}</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
     <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+      <button onclick="handleDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
     </div>
   </div>
 </div>
@@ -42,12 +44,12 @@ const displayPhones = (phones) => {
   handleLoading(false);
 };
 
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
   handleLoading(true);
   const searchInput = document.getElementById("search-input");
   const searchText = searchInput.value;
 
-  fetchAllData(searchText);
+  fetchAllData(searchText, isShowAll);
 };
 
 const handleLoading = (isLoading) => {
@@ -55,4 +57,39 @@ const handleLoading = (isLoading) => {
   if (isLoading) {
     loading.classList.remove("hidden");
   } else loading.classList.add("hidden");
+};
+
+const handleShowAll = () => {
+  handleSearch(true);
+};
+
+const handleDetails = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+  console.log(data.data);
+  const details = data.data;
+  const showDetails = document.getElementById("show_details");
+  showDetails.innerHTML = `
+        <div class="modal-box">
+        <img src="${details.image}" />
+          <h3 class="text-lg font-bold">${details.name}</h3>
+          <h4 class="text-l font-bold">Brand: ${details.brand}</h4>
+          <p class="py-1">Release Date: ${details.releaseDate}</p>
+          <h3 class="text-lg font-bold">More Information</h3>
+          <p class=""><span class="font-bold">Chipset</span> : ${details.mainFeatures.chipSet}</p>
+          <p class=""><span class="font-bold">Chipset</span> : ${details.mainFeatures.chipSet}</p>
+          <p class=""><span class="font-bold">Chipset</span> : ${details.mainFeatures.chipSet}</p>
+          <p class=""><span class="font-bold">Chipset</span> : ${details.mainFeatures.chipSet}</p>
+        
+          <div class="modal-action">
+            <form method="dialog">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn">Close</button>
+            </form>
+          </div>
+        </div>
+  `;
+  show_details.showModal();
 };
